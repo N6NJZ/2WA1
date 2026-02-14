0df9afd34eeb4e8969d20bb9b2d81140ed9174f4
David Richardson <dave@rv-7.com>
2026-02-14 10:38:24 -0800

    express static added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+app.arguments(express.static('assets'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 });
